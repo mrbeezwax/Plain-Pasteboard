e78ca1ad0caec99b7d06403f2f2118a479fda4ba
Juju Liao <liaojulian@gmail.com>
2020-01-08 17:33:41 -0800

    Initial Commit

---
+++ Plain Pasteboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plain Pasteboard
+//
+//  Created by Julian Liao on 1/8/20.
+//  Copyright © 2020 Julian Liao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
